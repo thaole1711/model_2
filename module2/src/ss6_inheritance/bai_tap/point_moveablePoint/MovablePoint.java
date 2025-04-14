5d8d71d1360e9b80ab81858a9418fcc18a2c0c86
unknown <le17112401@gmail.com>
2025-04-14 20:44:42 +0700

    da fix

--- module2/src/ss6_inheritance/bai_tap/point_moveablePoint/MovablePoint.java
+++ module2/src/ss6_inheritance/bai_tap/point_moveablePoint/MovablePoint.java
@@ -1,19 +1,21 @@
 package ss6_inheritance.bai_tap.point_moveablePoint;
 
-public class MovablePoint extends Point{
-    float xSpeed=0.0f;
-    float ySpeed=0.0f;
-    public MovablePoint(float x, float y,float xSpeed,float ySpeed) {
+public class MovablePoint extends Point {
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
         super(x, y);
-        this.xSpeed=xSpeed;
-        this.ySpeed=ySpeed;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
     }
 
     public MovablePoint(float xSpeed, float ySpeed) {
         this.xSpeed = xSpeed;
         this.ySpeed = ySpeed;
     }
-    public MovablePoint(){
+
+    public MovablePoint() {
     }
 
     public float getXSpeed() {
@@ -27,25 +29,28 @@ public class MovablePoint extends Point{
     public float getYSpeed() {
         return ySpeed;
     }
-    public void setYSpeed(float ySpeed){
-        this.ySpeed=ySpeed;
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
     }
 
     public void setSpeed(float xSpeed, float ySpeed) {
-        this.xSpeed=xSpeed;
-        this.ySpeed=ySpeed;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
     }
-    public float[]getSpeed(){
-        return new float[]{xSpeed ,ySpeed};
+
+    public float[] getSpeed() {
+        return new float[]{xSpeed, ySpeed};
     }
 
     @Override
     public String toString() {
-        return super.toString()+", speed =("+xSpeed+","+ySpeed+")";
+        return super.toString() + ", speed =(" + xSpeed + "," + ySpeed + ")";
     }
-    public MovablePoint move(){
-        super.setX(super.getX()+ getXSpeed());
-        super.setY(super.getY()+getYSpeed());
+
+    public MovablePoint move() {
+        super.setX(super.getX() + getXSpeed());
+        super.setY(super.getY() + getYSpeed());
         return this;
     }
 }
